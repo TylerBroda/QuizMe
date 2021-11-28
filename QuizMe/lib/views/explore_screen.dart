fa6dc22ca0c22f5db47300f9a87c2171020fa3ce
Tyler Broda <tylerbroda11@gmail.com>
2021-11-27 21:23:00 -0500

    Adding StreamBuilder to view uploaded quizzes in firebase DB

--- QuizMe/lib/views/explore_screen.dart
+++ QuizMe/lib/views/explore_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quizme/widgets/app_drawer.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 
 class ExploreScreen extends StatefulWidget {
@@ -10,14 +11,47 @@ class ExploreScreen extends StatefulWidget {
 }
 
 class _ExploreScreenState extends State<ExploreScreen> {
+  final quizzes = FirebaseFirestore.instance.collection('quizzes');
+
+  int _selectedIndex = -1;
+  String _docID = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Explore")),
       drawer: const AppDrawer(),
       resizeToAvoidBottomInset: false,
-      body: SafeArea(
-        child: Text('TODO: Uploaded Quiz sets & filter here'),
+      body: StreamBuilder(
+        stream: quizzes.snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) return const Text("No Data");
+          return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (BuildContext context, int index) {
+              final docData = snapshot.data!.docs[index].data() as Map;
+              return GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _selectedIndex = index;
+                    _docID = snapshot.data!.docs[index].id;
+                  });
+                },
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  child: Card(
+                    color: (_selectedIndex == index) ? Colors.blue : Colors.white,
+                    child: ListTile(
+                      title: Text(docData['Name'].toString()),
+                      subtitle: Text(docData['User'].toString()),
+                      trailing: const Icon(Icons.arrow_forward),
+                    ),
+                  )
+                )
+              );
+            }
+          );
+        },
       ),
     );
   }
