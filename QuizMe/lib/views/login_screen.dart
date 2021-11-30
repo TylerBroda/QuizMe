cabfe737c20c9e6ae3c5b4d419e1210c2c2c5e98
TalhaR-Sudo <Talha.rashid@ontariotechu.net>
2021-11-29 19:13:56 -0500

    unnecessary imports

--- QuizMe/lib/views/login_screen.dart
+++ QuizMe/lib/views/login_screen.dart
@@ -1,8 +1,5 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 // @dart=2.9
-import 'dart:async';
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:quizme/utils/app_colors.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
